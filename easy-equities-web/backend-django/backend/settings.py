3acc5ad8927cdab940d663f6aa4efd3f03c174c6
Mvuyiso-Harvinno <mvuyiso.gqwaru@harvinno.com>
2025-03-02 12:37:21 +0200

    Running web app

--- easy-equities-web/backend-django/backend/settings.py
+++ easy-equities-web/backend-django/backend/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'api',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -104,8 +105,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../../easy-equities-web/frontend/build/static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Add template directory for React
+TEMPLATES[0]['DIRS'] = [
+    os.path.join(BASE_DIR, '../../easy-equities-web/frontend/build')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
